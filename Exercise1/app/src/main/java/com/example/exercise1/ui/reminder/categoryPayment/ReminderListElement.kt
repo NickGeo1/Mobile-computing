a4fbedfde11debf4384f5ef7295470ffe427eb8d
NickGeo1 <nikosgeorgiadis2001@gmail.com>
2022-01-30 04:50:50 +0200

    Create ReminderListElement.kt

---
+++ Exercise1/app/src/main/java/com/example/exercise1/ui/reminder/categoryPayment/ReminderListElement.kt
@@ -0,0 +1,163 @@
+package com.example.exercise1.ui.reminder.categoryPayment
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.exercise1.ui.entities.Reminder
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Composable
+fun ReminderListElement()
+{
+    val viewModel: ReminderListViewModel = viewModel()
+    val viewState by viewModel.state.collectAsState()
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        ReminderList(
+            list = viewState.reminders
+        )
+    }
+}
+
+@Composable
+private fun ReminderList(
+    list: List<Reminder>
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(0.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        items(list) { item ->
+            ReminderListItem(
+                reminder = item,
+                onClick = {},
+                modifier = Modifier.fillParentMaxWidth(),
+            )
+        }
+    }
+}
+
+@Composable
+private fun ReminderListItem(
+    reminder: Reminder,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    ConstraintLayout(modifier = modifier.clickable { onClick() }) {
+        val (divider, id, title, icon, date) = createRefs()
+        Divider(
+            Modifier.constrainAs(divider) {
+                top.linkTo(parent.top)
+                centerHorizontallyTo(parent)
+                width = Dimension.fillToConstraints
+            }
+        )
+
+        // title
+        Text(
+            text = reminder.title,
+            maxLines = 1,
+            style = MaterialTheme.typography.subtitle1,
+            modifier = Modifier.constrainAs(title) {
+                linkTo(
+                    start = parent.start,
+                    end = icon.start,
+                    startMargin = 24.dp,
+                    endMargin = 16.dp,
+                    bias = 0f // float this towards the start. this was is the fix we needed
+                )
+                top.linkTo(parent.top, margin = 10.dp)
+                width = Dimension.preferredWrapContent
+            }
+        )
+
+        // id
+        Text(
+            text = reminder.id.toString(),
+            maxLines = 1,
+            style = MaterialTheme.typography.subtitle2,
+            modifier = Modifier.constrainAs(id) {
+                linkTo(
+                    start = parent.start,
+                    end = icon.start,
+                    startMargin = 24.dp,
+                    endMargin = 8.dp,
+                    bias = 0f // float this towards the start. this was is the fix we needed
+                )
+                top.linkTo(title.bottom, margin = 6.dp)
+                bottom.linkTo(parent.bottom, 10.dp)
+                width = Dimension.preferredWrapContent
+            }
+        )
+
+        // date
+        Text(
+            text = when {
+                reminder.date != null -> { reminder.date.formatToString() }
+                else -> Date().formatToString()
+            },
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            style = MaterialTheme.typography.caption,
+            modifier = Modifier.constrainAs(date) {
+                linkTo(
+                    start = id.end,
+                    end = icon.start,
+                    startMargin = 8.dp,
+                    endMargin = 16.dp,
+                    bias = 0f // float this towards the start. this was is the fix we needed
+                )
+                centerVerticallyTo(id)
+                top.linkTo(title.bottom, 6.dp)
+                bottom.linkTo(parent.bottom, 10.dp)
+            }
+        )
+
+        // icon for edit
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .size(50.dp)
+                .padding(6.dp)
+                .constrainAs(icon) {
+                    top.linkTo(parent.top, 10.dp)
+                    bottom.linkTo(parent.bottom, 10.dp)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Notifications,
+                contentDescription = "edit_btn"
+            )
+        }
+    }
+}
+
+private fun Date.formatToString(): String {
+    return SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault()).format(this)
+}
